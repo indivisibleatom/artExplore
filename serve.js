e056930c86d87ee689df233881e07373c082d5ac
Mukul Sati <mukulsati@gmail.com>
2013-03-29 14:45:42 -0400

    Adding static path handling

--- serve.js
+++ serve.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var url = require("url");
 var mysql = require("mysql");
+var path = require("path");
+var fs = require("fs");
 
 function start(route)
 {
@@ -27,9 +29,34 @@ function start(route)
       {
         throw err;
       }
-      response.write('The solutions is ' + rows[0].XML_URL);
-      response.end();
-    });
+      var uri = url.parse(request.url).pathname;
+      var filename = path.join(process.cwd()+"/multiImage/", uri);
+
+      path.exists(filename, function(exists) {
+        console.log("Fetching" + filename);
+        if(!exists) {
+          response.writeHead(404, {"Content-Type": "text/plain"});
+          response.write("404 Not Found\n");
+          response.end();
+          return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += 'basic1.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+          if(err) {        
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(err + "\n");
+            response.end();
+            return;
+          }
+
+          response.writeHead(200);
+          response.write(file, "binary");
+          response.end();
+        });
+     });
+   })
     
     //response.write(creds.hostname);
 
